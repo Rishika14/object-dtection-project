a9b4f4568f20aa46e7626c2a0e5a43707bf0916c
Rishika14 <74129664+Rishika14@users.noreply.github.com>
2021-05-22 17:04:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function toBedroom() {
+    window.location="bedroom.html"
+}
+function toTVandAC() {
+    window.location="tvandac.html"
+}
+function toDesk() {
+    window.location="desk.html"
+}
+function toBottles() {
+    window.location="bottles.html"
+}
+function toFruitbasket() {
+    window.location="fruitbasket.html"
+}
